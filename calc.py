50adef9f60c354d1b71ada2106e3272d031cea5b
Lakshmi Hari <phaneeswar456@gmail.com>
2019-09-04 00:05:27 +0530

    add test file calc

--- calc.py
+++ calc.py
@@ -2,10 +2,12 @@ def add(x,y):
         pass
 def subtract(x,y):
         pass
+        return x-y
 def multiply(x,y):
         return x*y
+        return x+y
 def divide(x,y):
         pass
 def square(x,y):
-        pass
+print   pass
 
